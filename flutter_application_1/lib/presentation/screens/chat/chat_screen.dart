08f00f51aa1cfd8a62b0eed81a914b43cb1c4467
Amisadai <210752@utxicotepec.edu.mx>
2024-11-22 01:29:12 -0600

    practica 23
    Modificacion y Agregacion de archivos para chat

--- flutter_application_1/lib/presentation/screens/chat/chat_screen.dart
+++ flutter_application_1/lib/presentation/screens/chat/chat_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/presentation/widgets/chat/my_message_bubble.dart';
-import 'package:flutter_application_1/presentation/widgets/chat/other_message_bubble.dart';
-import 'package:flutter_application_1/presentation/widgets/shared/message_field_box.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_application_1/domain/entities/message.dart';
 
+import 'package:flutter_application_1/presentation/providers/chat_provider.dart';
+import 'package:flutter_application_1/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:flutter_application_1/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:flutter_application_1/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -12,23 +15,25 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: const Padding(
-          padding:  EdgeInsets.all(4.0),
+          padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
             backgroundImage: NetworkImage(
-                'https://static.wixstatic.com/media/c14407_7e2c374ea7ba4b2abf7746aca4291973~mv2.jpg/v1/fill/w_640,h_480,fp_0.50_0.50,q_80,usm_0.66_1.00_0.01,enc_auto/c14407_7e2c374ea7ba4b2abf7746aca4291973~mv2.jpg'),
+                'https://i.pinimg.com/originals/08/bd/dc/08bddc4fb625f4fb52f0b8ebfe33dbd6.jpg'),
           ),
         ),
-        title: Text('Amy'),
+        title: const Text('Ami😺'),
         centerTitle: false,
       ),
       body: _ChatView(),
-    );         
+    );
   }
 }
 
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -36,20 +41,24 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
                 child: ListView.builder(
-              itemCount: 100,
-              itemBuilder: (context, index) {
-                return (index % 2 == 0 )
-                ? const OtherMessageBubble()
-                : const MyMessageBubble();
-              })),
-              
+                  controller: chatProvider.chatScrollController,
+                    itemCount: chatProvider.messageList.length,
+                    itemBuilder: (context, index) {
+                      final message = chatProvider.messageList[index];
+                       
+                      return (message.fromWho == FromWho.hers)
+                          ? HerMessageBubble( message: message )
+                          : MyMessageBubble( message: message );
+                    })),
 
-              // caja de texto de mensajes
-              const MessageFieldBox(),
+            /// Caja de texto de mensajes
+            MessageFieldBox(
+              // onValue: (value) => chatProvider.sendMessage(value),
+              onValue: chatProvider.sendMessage,
+            ),
           ],
         ),
       ),
     );
   }
 }
-            
